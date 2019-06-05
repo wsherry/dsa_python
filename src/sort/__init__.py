e2e45b5784193db03e7bb98309bc20ace7f7dc70
Ben Burk <benburk@outlook.com>
2019-06-05 18:45:46 -0400

    add tests for merge sort

--- src/sort/__init__.py
+++ src/sort/__init__.py
@@ -1,3 +1,4 @@
 """Sorting solutions"""
 from .insertion_sort import *
+from .merge_sort import *
 from .selection_sort import *
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,5 +1,10 @@
 """test sorting alogrithms"""
-from src.sort import insertion_sort, insertion_sort_optimized, selection_sort
+from src.sort import (
+    insertion_sort,
+    insertion_sort_optimized,
+    selection_sort,
+    merge_sort,
+)
 
 
 def test_insertion_sort():
@@ -19,5 +24,12 @@ def test_insertion_sort_optimized():
 def test_selection_sort():
     """test selection sort"""
     actual = selection_sort([1, 5, 65, 23, 57, 1232])
+    expected = [1, 5, 23, 57, 65, 1232]
+    assert actual == expected
+
+
+def test_merge_sort():
+    """test merge sort"""
+    actual = merge_sort([1, 5, 65, 23, 57, 1232])
     expected = [1, 5, 23, 57, 65, 1232]
     assert actual == expected
