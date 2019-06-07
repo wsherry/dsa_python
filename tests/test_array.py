f36c63f1bd9b6a81ebcaf0f4d99011611613b86a
Ben Burk <benburk@outlook.com>
2019-06-07 14:43:51 -0400

    change test_two_sum to use for loop

--- tests/test_array.py
+++ tests/test_array.py
@@ -21,17 +21,20 @@ def test_two_sum_naive():
 
 def test_two_sum_dict():
     """test two sum dict"""
-    actual = two_sum_dict([2, 11, 7, 9], target=9)
-    expected = (0, 2)
-    assert actual == expected
+    test_cases = (
+        ([2, 11, 7, 9], 9, (0, 2)),
+        ([-3, 5, 2, 3, 8, -9], 0, (0, 3)),
+        ([-3, 5, 2, 3, 8, -9], 6, None),
+    )
 
-    actual = two_sum_dict([-3, 5, 2, 3, 8, -9], target=0)
-    expected = (0, 3)
-    assert actual == expected
+    for args in test_cases:
+        actual = two_sum_dict(*args[:-1])
+        expected = args[-1]
+        assert actual == expected
 
-    actual = two_sum_dict([-3, 5, 2, 3, 8, -9], target=6)
-    expected = None
-    assert actual == expected
+        actual = two_sum_naive(*args[:-1])
+        expected = args[-1]
+        assert actual == expected
 
 
 def test_find_peak():
@@ -44,5 +47,5 @@ def test_find_peak():
     )
     for args in test_cases:
         actual = find_peak(*args[:-1])
-        expected = args[1]
+        expected = args[-1]
         assert actual == expected
